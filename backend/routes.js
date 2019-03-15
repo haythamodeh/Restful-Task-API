29477a0fa03ea4421c7fc19ad3135d584bd2d710
prescottbreeden <prescott.breeden@gmail.com>
2019-03-14 17:20:18 -0700

    <Title> Code Review with Prescott
    <Description/Comment> Talked about style guides for JavaScript, edited 4
    files to follow better practices, fixed some of the mongoose methods in
    the controller.js to remove potentially deprecated mongoose functions.

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,10 @@
 const controller = require("./controller");
 
-module.exports = function (app){
-    app
-        .get('/', controller.allTasks)
-        .get('/api/task/:id', controller.getTask)
-        .post("/api/task", controller.createTask)
-        .delete("/api/task/delete/:id", controller.deleteTask)
-        .put("/api/task/:id", controller.updateTask)
+module.exports = app => {
+  app
+    .get('/api/tasks', controller.allTasks)
+    .get('/api/tasks/:id', controller.getTask)
+    .post('/api/tasks', controller.createTask)
+    .put('/api/tasks/:id', controller.updateTask)
+    .delete('/api/tasks/:id', controller.deleteTask);
 }
