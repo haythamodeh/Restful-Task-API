c4353f305e6dae63ec5ecbe613ef345c818404a9
haythamodeh <HOdehLion@Haythams-MacBook-Air.local>
2019-03-15 14:39:09 -0700

    added pokemon api

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -5,24 +5,35 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpService {
-  allTasks() {
-    let tempObservable = this.http.get("/api/tasks");
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  // allTasks() {
+  //   let tempObservable = this.http.get("/api/tasks");
+  //   tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  // }
+  // getTask() {
+  //   var id: any = "5c89a9ef91d28f0bedc40f61";
+  //   let tempObservable = this.http.get("/api/tasks/" + id);
+  //   tempObservable.subscribe(data => console.log("Got one task!", data));
+  // }
+  // deleteTask() {
+  //   var id: any = "5c89a9ef91d28f0bedc40f61";
+  //   let tempObservable = this.http.delete("/api/tasks/" + id);
+  //   tempObservable.subscribe(data => console.log("Deleted Task!", data));
+  // }
+  getPokemon(){
+    let bulbasaur = this.http.get('https://pokeapi.co/api/v2/pokemon/1');
+    bulbasaur.subscribe(data => console.log("Got Pokemon", data));
   }
-  getTask() {
-    var id: any = "5c89a9ef91d28f0bedc40f61";
-    let tempObservable = this.http.get("/api/tasks/" + id);
-    tempObservable.subscribe(data => console.log("Got one task!", data));
-  }
-  deleteTask() {
-    var id: any = "5c89a9ef91d28f0bedc40f61";
-    let tempObservable = this.http.delete("/api/tasks/" + id);
-    tempObservable.subscribe(data => console.log("Deleted Task!", data));
+  getPokemonAbility() {
+    let ability = this.http.get('https://pokeapi.co/api/v2/ability/34');
+    ability.subscribe(data => console.log("Abilities", data));
   }
 
+
   constructor(private http: HttpClient) { 
-    this.allTasks();
-    this.getTask();
-    this.deleteTask();
+    // this.allTasks();
+    // this.getTask();
+    // this.deleteTask();
+    this.getPokemon();
+    this.getPokemonAbility();
   }
 }
